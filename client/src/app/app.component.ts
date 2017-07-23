04853124b4bb0c5aa52da3dd0900feb592021bb7
Alex Sicart Ramos <alex@sharge.io>
2017-07-23 09:12:22 +0200

    Initial commit

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {Observable} from 'rxjs';
+import '../../../imports/api/files';
+
+@Component({
+  selector: 'app-root',
+  template: '<ul><li *ngFor="let item of myData | async"> {{ item }} </li></ul>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  myData: Observable<any>;
+  constructor() {
+    this.myData = myFiles.find({});
+  }
+}
---
+++ imports/api/files.js
@@ -0,0 +1,3 @@
+import {MongoObservable} from 'meteor-rxjs';
+
+export const myFiles = MongoObservable.Collection('files');
