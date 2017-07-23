20f33c6d9874380593466946d5cf14b00d6947c1
Alex Sicart Ramos <alex@sharge.io>
2017-07-23 09:21:20 +0200

    feat: Server connected

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {Observable} from 'rxjs';
 import '../../../imports/api/files';
+import angularMeteor from 'angular-meteor';
+
 
 @Component({
   selector: 'app-root',
